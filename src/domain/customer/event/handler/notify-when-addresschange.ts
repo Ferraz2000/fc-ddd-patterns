cac5bbe2f23a82f6a778db4a91f5f646cb97f5e9
Ferraz2000 <lucas.devmoraes@gmail.com>
2024-04-09 10:24:39 -0300

    fix: notifywhenaddresschange

--- src/domain/customer/event/handler/notify-when-addresschange.ts
+++ src/domain/customer/event/handler/notify-when-addresschange.ts
@@ -1,9 +1,9 @@
 import EventHandlerInterface from "../../../@shared/event/event-handler.interface";
 import CustomerChangeAddressEvent from "../customer-changeaddress.event";
-import CustomerCreatedEvent from "../customer-created.event";
+
 
 export default class NotifyWhenAddressChangeHandler
-implements EventHandlerInterface<CustomerCreatedEvent>
+implements EventHandlerInterface<CustomerChangeAddressEvent>
 {
     handle(event: CustomerChangeAddressEvent): void {
         console.log(`Endereço do cliente: ${event.eventData.id}, ${event.eventData.name} alterado para: ${event.eventData.Address.toString()}`);
